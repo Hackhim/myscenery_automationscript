df60c5441f0a1778eef56c7f6421baf87726d2f7
JeremyLP <jeremyleprunenec.pro@gmail.com>
2023-06-01 14:33:51 +0200

    Disable launch prints in group

--- launchprints.py
+++ launchprints.py
@@ -5,7 +5,7 @@ from farm.farm import Farm
 def main():
     farm = Farm()
     farm.launch_prints()
-    farm.launch_prints_for_printers_in_group()
+    # farm.launch_prints_for_printers_in_group()
 
     if len(farm.launched_prints) > 0:
         print(f"<{datetime.datetime.now()}> LAUNCHED PRINTS:")
